4676cce78f6411de2943353c952b48cf6afb72fb
Pha1Huynh <huynhtanphatleo2gmail.com>
2023-05-12 16:49:46 +0700

    complete infoHeader UI

--- src/components/InfoHeader/InfoHeader.tsx
+++ src/components/InfoHeader/InfoHeader.tsx
@@ -1,17 +1,51 @@
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-
-import { faUser } from '@fortawesome/free-solid-svg-icons';
 import './InfoHeader.scss';
+
+import {
+  faPinterest,
+  faTwitter,
+  faFacebook,
+  faDribbble,
+  faBehance,
+  faLinkedinIn,
+  faFacebookF,
+} from '@fortawesome/free-brands-svg-icons';
 function InfoHeader() {
   return (
     <>
       <div className="infoHeader-container">
-        <p className="phone-info"></p>
-        <div className="social-info">
-          <Link to="">
-            <FontAwesomeIcon icon={faUser} className="user-icon" />
-          </Link>
+        <div className="max-wide-1200">
+          <div className="left">
+            <p className="phone-info">+84 945 000 917</p>
+            <div className="social-info">
+              <Link to="">
+                <FontAwesomeIcon icon={faPinterest} className="social-icon" />
+              </Link>
+              <Link to="">
+                <FontAwesomeIcon icon={faFacebookF} className="social-icon" />
+              </Link>
+              <Link to="">
+                <FontAwesomeIcon icon={faTwitter} className="social-icon" />
+              </Link>
+              <Link to="">
+                <FontAwesomeIcon icon={faDribbble} className="social-icon" />
+              </Link>
+              <Link to="">
+                <FontAwesomeIcon icon={faBehance} className="social-icon" />
+              </Link>
+              <Link to="">
+                <FontAwesomeIcon icon={faLinkedinIn} className="social-icon" />
+              </Link>
+            </div>
+          </div>
+          <div className="right">
+            <div className="login-and-register">
+              <Link to="">Login</Link>
+              <div className="stick"></div>
+              <Link to="">Register</Link>
+            </div>
+          </div>
         </div>
       </div>
     </>
