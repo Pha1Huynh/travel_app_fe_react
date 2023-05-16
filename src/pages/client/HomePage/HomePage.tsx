7a66c14c819ae032e52aa9d83304a86d70042704
Pha1Huynh <huynhtanphatleo2gmail.com>
2023-05-16 15:38:55 +0700

    add button component

--- src/pages/client/HomePage/HomePage.tsx
+++ src/pages/client/HomePage/HomePage.tsx
@@ -3,12 +3,14 @@ import './HomePage.scss';
 import AppHeader from '../../../components/AppHeader/AppHeader';
 import HomeBanner from '../../../images/banner/home.webp';
 import SearchTour from '../../../components/SearchTour/SearchTour';
+
 const HomePage = () => {
   return (
     <>
       <div className="app-container">
         <AppHeader image={HomeBanner} firstText="Discover" secondText="the world" isHaveButton={true} />
         <SearchTour />
+
         <div className="main-content"></div>
       </div>
     </>
