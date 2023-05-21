ee3b5580138fa5242e6731aa2146fa2fef23e2cd
NguyenThanh1709 <nvothanh00@gmail.com>
2023-05-21 11:53:40 +0700

    ValidateInput

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,3 +1,6 @@
 import HomePage from '../pages/client/HomePage/HomePage';
 import LoginPage from '../pages/client/LoginPage/LoginPage';
-export const publicRoutes = [{ path: '/', component: HomePage },{ path: '/login', component: LoginPage }];
+// import RegisterPage from '../pages/client/RegisterPage/RegisterPage';
+export const publicRoutes = [
+{ path: '/', component: HomePage },
+{ path: '/login', component: LoginPage },];
