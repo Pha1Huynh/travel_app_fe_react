ff3eaba0b9472cabcc36497e42e9ef5cd65a0988
Pha1Huynh <huynhtanphatleo2gmail.com>
2023-05-22 20:02:20 +0700

    complete testimonials

--- src/components/HomePageComponents/Testimonials/Testimonials.tsx
+++ src/components/HomePageComponents/Testimonials/Testimonials.tsx
@@ -1,4 +1,7 @@
 import TestimonialItem from '../../TestimonialItem/TestimonialItem';
+import tripImg from '../../../images/icons/trip.webp'
+import customerImg from '../../../images/testimonials/testimonial1.webp'
+import './Testimonials.scss'
 function Testimonials() {
   return (
     <>
@@ -6,7 +9,10 @@ function Testimonials() {
         <div className="max-wide-1200">
           <p className="title">WHAT OUR CLIENTS SAY ABOUT US</p>
           <div className="testimonials-list">
-            <TestimonialItem />
+            <TestimonialItem image={customerImg} icon={tripImg} dateTime='May 24, 2020' customerName='CARLA SMITH' title='Best holliday ever' description='Nullam eu convallis tortor. Suspendisse potenti. In faucibus massa arcu, vitae cursus mi hendrerit nec.' />
+            <TestimonialItem image={customerImg} icon={tripImg} dateTime='May 24, 2020' customerName='CARLA SMITH' title='Best holliday ever' description='Nullam eu convallis tortor. Suspendisse potenti. In faucibus massa arcu, vitae cursus mi hendrerit nec.' />
+            <TestimonialItem image={customerImg} icon={tripImg} dateTime='May 24, 2020' customerName='CARLA SMITH' title='Best holliday ever' description='Nullam eu convallis tortor. Suspendisse potenti. In faucibus massa arcu, vitae cursus mi hendrerit nec.' />
+
           </div>
         </div>
       </div>
--- src/components/TestimonialItem/TestimonialItem.tsx
+++ src/components/TestimonialItem/TestimonialItem.tsx
@@ -1,6 +1,22 @@
+import { ITestimonialItem } from '../../interfaces/components/testimonials.interface';
 import './TestimonialItem.scss';
-function TestimonialItem() {
-  return <>This is TestimonialItem</>;
+function TestimonialItem(props: ITestimonialItem) {
+  const { image, icon, customerName, dateTime, title, description } = props
+  return <>
+    <div className='testimonialsItem-container' style={{ backgroundImage: `url(${image})` }}>
+      <div className='icon'><img src={icon} alt='' /></div>
+      <div className='customer-info'>
+        <p className='name'>{customerName}</p>
+        <p className='dateTime'>{dateTime}</p>
+      </div>
+      <div className='customer-comment'>
+        <p className='customer-comment-title'>" {title} "</p>
+        <p className='customer-comment-description'>{description}</p>
+      </div>
+      <div className='bottom-background'></div>
+
+    </div>
+  </>;
 }
 
 export default TestimonialItem;
